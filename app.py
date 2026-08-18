74982e55acabc7d189158398aad0cb18fd22b367
samidha <samidhawani7@gmail.com>
2026-08-18 18:09:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import mysql.connector
+import os 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -12,10 +13,11 @@ app.secret_key = "3aa7b16253eb2885c5ad6d5cf89dfbd02507335701bb2b3140ea319eb0a391
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="mysql",
-        user="root",
-        password="root",
-        database="task_manager"
+        host=os.getenv("DB_HOST", "host.docker.internal"),
+        port=int(os.getenv("DB_PORT", "3307")),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", "root123"),
+        database=os.getenv("DB_NAME", "task_manager")
     )
 
 
