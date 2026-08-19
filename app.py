e20ba0b2602f8ca3d019c0e3fbbb7b0bc2c8c9ef
samidha <samidhawani7@gmail.com>
2026-08-19 13:17:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="task_manager"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
+
 
 
 # -------------------------
